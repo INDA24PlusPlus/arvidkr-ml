615a5fe560880511c0fcf785d7cb70d0e3da5cc0
Arvid Kristoffersson <arvid.kristoffersson@icloud.com>
2024-11-20 16:10:39 +0100

    76%

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@
 #include <string>
 #include <iomanip>
 #include <algorithm>
+#include <chrono>
+
+//chrono::time_point<std::chrono::system_clock> start;
+//start = std::chrono::system_clock::now();
+//std::chrono::duration<double> elapsed_seconds = end - start;
+//elapsed_second.count();
 
 #define all(x)  (x).begin(), (x).end()
 #define rall(x)  (x).rbegin(), (x).rend()
@@ -57,7 +63,7 @@ vector<double> weirdProduct(vector<double> v1, vector<double> v2){
     return v3;
 }
 
-vector<vector<double>> nmatr(vector<double> v1, vector<double> v2){
+vector<vector<double>> nmatr(vector<double>& v1, vector<double>& v2){
     vector<vector<double> > ret;
     for (int i = 0; i < v1.size(); i++){
         ret.push_back({});
@@ -174,7 +180,7 @@ struct Network{
         return ret;
     }
 
-    void update(int layer_no, vector<vector<double>> weight_proposals, vector<double> bias_proposals, double alpha){
+    void update(int layer_no, vector<vector<double>>& weight_proposals, vector<double>& bias_proposals, double alpha){
         for (int i = 0; i < weight_proposals.size(); i++){
             for (int j = 0; j < weight_proposals[0].size(); j++){
                 layers[layer_no].weights[i][j] -= alpha*weight_proposals[i][j];
@@ -184,7 +190,7 @@ struct Network{
         for (int i = 0; i < bias_proposals.size(); i++)layers[layer_no].biases[i] -= alpha*bias_proposals[i];
     }
 
-    void back_propagation(vector<pair<vector<double>, vector<double>>> from_forward, vector<double> X, vector<double> Y, double batch_size, double learning_rate){
+    void back_propagation(vector<pair<vector<double>, vector<double>>>& from_forward, vector<double>& X, vector<double>& Y, double batch_size, double learning_rate){
         vector<double> dz;
         vector<vector<double>> weights;
         vector<pair<vector<vector<double> >, vector<double>>> ret;
@@ -209,9 +215,9 @@ struct Network{
 
 };
 
-void train(Network& Net, vector<vector<double>> train_x, vector<pair<double, double> > train_y, int batch_size, double learning_rate){
+void train(Network& Net, vector<vector<double>>& train_x, vector<vector<double> >& train_y, int batch_size, double learning_rate){
     vector<vector<double>> ac_x;
-    vector<pair<double, double>> ac_y;
+    vector<vector<double>> ac_y;
     for (int i = 0; i < batch_size; i++){
         int j = rand()%(train_x.size());
         ac_x.push_back(train_x[j]);
@@ -220,10 +226,22 @@ void train(Network& Net, vector<vector<double>> train_x, vector<pair<double, dou
 
     vector<pair<vector<vector<double>>, vector<double>>> suggestions;
 
-
     for (int i = 0; i < batch_size; i++){
+        chrono::time_point<chrono::system_clock> starti = chrono::system_clock::now();
         vector<pair<vector<double>, vector<double>>> from_forward = Net.forward(ac_x[i]);
-        Net.back_propagation(from_forward, ac_x[i], {ac_y[i].first, ac_y[i].second}, batch_size, learning_rate);
+        chrono::time_point<chrono::system_clock> endi = chrono::system_clock::now();
+
+        chrono::duration<double> elapsed_seconds = endi - starti;
+        //cout << "forward_time: " << elapsed_seconds.count() << endl;
+        
+        Net.back_propagation(from_forward, ac_x[i], ac_y[i], batch_size, learning_rate);
+
+        chrono::time_point<chrono::system_clock> endi2 = chrono::system_clock::now();
+
+        elapsed_seconds = endi2 - endi;
+        //cout << "back_time: " << elapsed_seconds.count() << endl;
+
+
     }
 
 }
@@ -241,47 +259,129 @@ double evaluate(Network& Net, vector<vector<double>> X, vector<vector<double>> Y
     return ((sum)/((double)X.size()));
 }
 
+double correcti(Network& Net, vector<vector<double>>& X, vector<vector<double>>& Y){
+    double correct = 0;
+    double total = 0;
+    int counter = 0;
+    for (auto e : X){
+        vector<double> returni = Net.forward(e).back().second;
+        /*cout << "Y: ";
+        for (auto li : Y[counter])cout << li << " ";
+        cout << endl;
+        cout << "returni: ";
+        for (auto li : returni)cout << li << " ";
+        cout << endl;*/
+        int guess = 0;
+        double guess_s = 0, actual_s = 0;
+        int actual = 0;
+        for (int i = 0; i < returni.size(); i++){
+            if (returni[i] > guess_s){
+                guess_s = returni[i];
+                guess = i;
+            }
+            if (Y[counter][i] > actual_s){
+                actual_s = Y[counter][i];
+                actual = i;
+            }
+        }
+
+        correct += (guess == actual);
+        total += 1;
+        counter++;
+    }
+    return correct/total;
+}
+
 int main(){
-    //odd or not
-    srand(420);
+    //MNIST
+    srand(42);
     vector<vector<double>> train_x;
-    vector<pair<double, double> > train_y;
-    for (int i = 0; i < 8; i++){
-        vector<double> temp = {0, 0, 0, 0, 0, 0, 0, 0};
-        temp[i] = 1;
-        train_x.push_back(temp);
-        train_y.push_back({i%2, (i+1)%2});
-    }    
+    vector<vector<double>> train_y;
+    vector<vector<double>> test_x;
+    vector<vector<double>> test_y;
+
+    freopen("mnist_train.csv", "r", stdin);
+
+    string line; 
+    int counter = 0;
+    while (cin >> line){
+        if (line == "-1")break;
+        train_x.push_back({});
+        string s = "";
+        bool is_first = true;
+        for (int i = 0; i < line.size(); i++){
+            if (line[i] == ','){
+                if (is_first){
+                    vector<double> temp_y = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+                    temp_y[stoi(s)] = 1.0;
+                    train_y.push_back(temp_y);
+                    is_first = false;
+                }
+                else train_x[counter].push_back(stoi(s));
+                s = "";
+            }
+            else {
+                s += line[i];
+            }
+        }
+        train_x[counter++].push_back(stoi(s));
+    }
+
+    line = "";
+    counter = 0;
+
+    while (cin >> line){
+        test_x.push_back({});
+        string s = "";
+        bool is_first = true;
+        for (int i = 0; i < line.size(); i++){
+            if (line[i] == ','){
+                if (is_first){
+                    vector<double> temp_y = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+                    temp_y[stoi(s)] = 1.0;
+                    test_y.push_back(temp_y);
+                    is_first = false;
+                }
+                else test_x[counter].push_back(stoi(s));
+                s = "";
+            }
+            else {
+                s += line[i];
+            }
+        }
+        test_x[counter++].push_back(stoi(s));
+    }
+
 
     vector<pair<string, pair<int, int> > > v;
-    v.push_back({"fc-layer", {8, 8}});
+    v.push_back({"fc-layer", {784, 10}});
     v.push_back({"sigmoid", {0,  0}});
-    v.push_back({"fc-layer", {8, 8}});
-    v.push_back({"sigmoid", {0,  0}});
-    v.push_back({"fc-layer", {8, 2}});
+    v.push_back({"fc-layer", {10, 10}});
     v.push_back({"softmax", {0,  0}});
+    //v.push_back({"fc-layer", {32, 10}});
+    //v.push_back({"softmax", {0,  0}});
 
     Network Net(v);
-    vector<double> forward_ret = Net.forward(train_x[0]).back().second;
 
+    cout << train_y[0][5] <<  " " << train_x[0].size() << endl;
+    cout << test_y[0][7] <<  " " << test_x[0].size() << endl;
 
-    int amount_of_epochs = 10000, batch_size = 16;
+    int amount_of_epochs = 10000, batch_size = 128;
     double learning_rate = 0.15;
     for (int i = 0; i < amount_of_epochs; i++){
-        if (i%100 == 0){
+        if (i%1000 == 0){
+            vector<vector<double>> temp_x;
             vector<vector<double>> temp_y;
-            for (int j = 0; j < train_y.size(); j++)temp_y.push_back({train_y[j].first, train_y[j].second});
-            cout << "After " << i << " epochs: " << evaluate(Net, train_x, temp_y) << endl;
+            for (int j = 0; j < 1000; j++){
+                temp_x.push_back(test_x[j]);
+                temp_y.push_back(test_y[j]);
+            }
+
+            cout << "After " << i << " epochs: " << evaluate(Net, temp_x, temp_y) << "error" << " correctness:" << correcti(Net, temp_x, temp_y) << endl;
         }
         train(Net, train_x, train_y, batch_size, learning_rate);
     }
 
-
-    int counter = 0;
-    for (vector<double> x : train_x){
-        forward_ret = Net.forward(x).back().second;
-        cout << ++counter << " " << forward_ret[0] << " " << forward_ret[1] << endl;
-    }
 
 
     return 0;
